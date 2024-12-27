1eaba8ff46998c349ff8216252f8691ccfe681a2
Jolt-rex <leepotter010@gmail.com>
2024-12-27 18:26:16 +1000

    Completed question 3

--- Question3.py
+++ Question3.py
@@ -6,53 +6,63 @@
 
 from turtle import *
 
-
-def draw_tree(recursion_depth, left_angle, right_angle, start_length, length_reduction_factor):
-    draw_trunk(start_length)
-
-    color('green')
-    width(2)
-    draw_branch(1, recursion_depth, True, left_angle, right_angle, start_length, length_reduction_factor)
-    draw_branch(1, recursion_depth, False, left_angle, right_angle, start_length, length_reduction_factor)
-
-def draw_trunk(start_length):
+# moves turtle position lower down window to give more room for drawing tree
+# and set angle ready to draw vertical branch
+def move_to_start():
     up()
     right(90)
     forward(200)
     left(180)
     down()
-    color('brown')
-    width('4')
-    forward(start_length)
 
-def draw_branch(current_depth, recursion_depth, is_left, left_angle, right_angle, length, reduction_factor):
+# move to given position and orientation without drawing
+def move_position(p, o):
+    up()
+    goto(p)
+    setheading(o)
+    down()
+
+# recursive function
+def draw_branch(recursion_depth, left_angle, right_angle, length, reduction_factor, current_depth=0, is_left=False, branch_width=10):
     if current_depth >= recursion_depth:
         return
+    
+    width(branch_width)
+    
+    # draw the branch
+    if current_depth == 0:
+        color('brown')
+    else:
+        color('green')
+        left(left_angle) if is_left else right(right_angle)
+    forward(length)
 
     current_position = position()
     current_heading = heading()
     
-    left(left_angle) if is_left else right(right_angle)
-    forward(length)
+    next_length =  length * (reduction_factor / 100)
+    next_width = branch_width * (reduction_factor / 100)
+    # ensure we always have width of at least 1 pixel
+    if next_width < 1: next_width = 1
 
-    next_length =  length # - length * (reduction_factor / 100)
-
-    draw_branch(current_depth + 1, recursion_depth, True, left_angle, right_angle, next_length, reduction_factor)
-    goto(current_position)
-    setheading(current_heading)
-    draw_branch(current_depth + 1, recursion_depth, False, left_angle, right_angle, next_length, reduction_factor)
-
+    # draw left and right branches recursively, resetting turtle position between function calls
+    draw_branch(recursion_depth, left_angle, right_angle, next_length, reduction_factor, current_depth + 1, True, next_width)
+    move_position(current_position, current_heading)
+    draw_branch(recursion_depth, left_angle, right_angle, next_length, reduction_factor, current_depth + 1, False, next_width)
+    
 
 ##### main entry point #####
+left_angle = float(input('Left branch angle (degrees): '))
+right_angle = float(input('Right branch angle (degrees): '))
+start_length = float(input('Starting branch length (pixels): '))
+recursion_depth = int(input('Recursion depth: '))
+length_reduction_factor = float(input('Branch length reduction factor (percent): '))
 
-left_angle = 20 # float(input('Left branch angle: '))
-right_angle = 25 # float(input('Right branch angle: '))
-start_length = 100 # float(input('Starting branch length: '))
-recursion_depth = 5 # int(input('Recursion depth: '))
-length_reduction_factor = 70 # float(input('Branch length reduction factor %: '))
+# move starting position partway down the window to space tree more evenly
+move_to_start()
 
+# draw the tree using recursive function
+draw_branch(recursion_depth, left_angle, right_angle, start_length, length_reduction_factor)
 
-draw_tree(recursion_depth, left_angle, right_angle, start_length, length_reduction_factor)
-
-
+# pause to display image
 mainloop()
